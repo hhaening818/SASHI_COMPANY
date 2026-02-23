9d10e50a3d866e522d44df2af5e0ca0869b7e45c
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-23 12:23:50 +0900

    update files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 from flask import Flask
 
@@ -9,3 +10,16 @@ def hello():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='127.0.0.1', port=port)
+=======
+import os
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='127.0.0.1', port=port)
+>>>>>>> c52eacd691e7861981f5c2dd529a28487e036fba
