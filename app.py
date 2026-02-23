edb6036b2e2d4fc7b5225f943e574be368f329c1
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-23 12:39:33 +0900

    homepage complete

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='127.0.0.1', port=port)
-=======
-import os
-from flask import Flask
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='127.0.0.1', port=port)
->>>>>>> c52eacd691e7861981f5c2dd529a28487e036fba
+if __name__ == "__main__":
+    app.run(debug=True)
