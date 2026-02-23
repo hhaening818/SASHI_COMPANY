eaa3a6d51df373ec1b33f706a3754dcf980e618c
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-23 12:07:16 +0900

    버전 변경

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='127.0.0.1', port=port)
