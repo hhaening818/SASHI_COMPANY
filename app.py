fea8e8a860c8b24bb158b3af3ccc7233deb704e4
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-23 13:05:26 +0900

    portfolio

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 메인
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# 시공사례
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
